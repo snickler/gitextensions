29972b2f4be7127d130a3b0e735e3ef36772c1a9
RussKie <russkie@gmail.com>
2019-12-18 00:13:33 +1100

    fixup! Convert projects to SDK-style

--- UnitTests/Plugins/BuildServerIntegration/AppVeyorIntegrationTests/Properties/AssemblyInfo.cs
+++ UnitTests/Plugins/BuildServerIntegration/AppVeyorIntegrationTests/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Runtime.InteropServices;
 using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
+using ApprovalTests.Reporters.TestFrameworks;
+using ApprovalTests.Reporters.ContinuousIntegration;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
