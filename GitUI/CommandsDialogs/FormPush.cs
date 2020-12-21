86ca6834b2384c8b26f694b6ee566337e2f547c7
RussKie <russkie@gmail.com>
2020-12-21 15:42:13 +1100

    Convert .NET 5.0

--- GitUI/CommandsDialogs/FormPush.cs
+++ GitUI/CommandsDialogs/FormPush.cs
@@ -643,7 +643,7 @@ namespace GitUI.CommandsDialogs
 
                 int dialogResult = -1;
 
-                using var dialog = new TaskDialog
+                using var dialog = new Microsoft.WindowsAPICodePack.Dialogs.TaskDialog
                 {
                     OwnerWindowHandle = owner.Handle,
                     Text = allOptions ? _pullRepositoryMergeInstruction.Text : _pullRepositoryForceInstruction.Text,
@@ -653,7 +653,7 @@ namespace GitUI.CommandsDialogs
                     Icon = TaskDialogStandardIcon.Error,
                     FooterCheckBoxText = _dontShowAgain.Text,
                     FooterIcon = TaskDialogStandardIcon.Information,
-                    StartupLocation = TaskDialogStartupLocation.CenterOwner,
+                    StartupLocation = Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStartupLocation.CenterOwner,
                     Cancelable = true
                 };
                 var btnPullDefault = new TaskDialogCommandLink("PullDefault", null, pullDefaultButtonText);
